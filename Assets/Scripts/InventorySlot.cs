ac814f6e5cd7ab267b2399d7d6f62950233cb0c9
Merc1073 <sergeikoblov2000@gmail.com>
2023-07-09 19:10:08 +0100

    Draggable inventory items

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag;
+        InventoryItem draggableItem = dropped.GetComponent<InventoryItem>();
+        draggableItem.parentAfterDrag = transform;
+    }
+
+}
